7c617fa2ed62745b0a564c6545a91096e8032789
behradsiamaki <145423707+behradsiamaki@users.noreply.github.com>
2024-01-02 12:02:15 +0330

    add active property for next and prev buttton

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const progress = document.getElementById(".progress");
-const prev = document.getElementById(".prev");
-const next = document.getElementById(".next");
-const circles = document.querySelectorAll(".ciecle");
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
 
 let currentActive = 1;
 
@@ -22,11 +22,23 @@ prev.addEventListener("click", () => {
 });
 
 function update() {
-    for(let[idx, circle] of circles.entres()){
-        if(idx < currentActive){
-            circle.classList.add('active');
-        }else{
-            circle.classList.remove('active');
-        }
+  for (let [idx, circle] of circles.entries()) {
+    if (idx < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
     }
+  }
+
+  const actives = document.querySelectorAll(".active");
+  progress.style.width = (((actives.length - 1) / (circles.length - 1)) * 100 + '%');
+
+  if (currentActive === 1) {
+    prev.disabled = true;
+  } else if (currentActive === circles.length) {
+    next.disabled = true;
+  } else {
+    next.disabled = false;
+    prev.disabled = false;
+  }
 }
